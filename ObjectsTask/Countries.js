71c92ed8303187beabdc72a901a38fe207738bd4
aPermogortseva <dm3gGjPZQ67Tmke>
2022-02-02 19:18:07 +0700

    Внесены исправления в задачу Работа с объектами

--- ObjectsTask/Countries.js
+++ ObjectsTask/Countries.js
@@ -1,94 +1,101 @@
 (function () {
-    var countries = [];
+        var countries = [
+            {
+                name: "Россия",
+                cities: [
+                    {
+                        name: "Новосибирск",
+                        population: 1511000
+                    },
+                    {
+                        name: "Омск",
+                        population: 1159000
+                    },
+                    {
+                        name: "Красноярск",
+                        population: 1007000
+                    }
+                ]
+            },
+            {
+                name: "Англия",
+                cities: [
+                    {
+                        name: "Ливерпуль",
+                        population: 496784
+                    },
+                    {
+                        name: "Манчестер",
+                        population: 553230
+                    },
+                    {
+                        name: "Лондон",
+                        population: 8982000
+                    },
+                    {
+                        name: "Бристоль",
+                        population: 428234
+                    }
+                ]
+            },
+            {
+                name: "Италия",
+                cities: [
+                    {
+                        name: "Палермо",
+                        population: 657960
+                    },
+                    {
+                        name: "Милан",
+                        population: 1396059
+                    },
+                    {
+                        name: "Милан",
+                        population: 1396059
+                    },
+                    {
+                        name: "Милан",
+                        population: 1396059
+                    }
+                ]
+            }
+        ];
 
-    countries[0] = {
-        name: "Россия",
-        cities: []
-    }
+        function getMaxCitiesCountCountries() {
+            var maxCitiesCount = countries.reduce(function (max, current) {
+                if (current.cities.length > max) {
+                    max = current.cities.length;
+                }
 
-    countries[0].cities[0] = {
-        name: "Новосибирск",
-        population: 1511000
-    }
+                return max;
+            }, 0);
 
-    countries[0].cities[1] = {
-        name: "Омск",
-        population: 1159000
-    }
-
-    countries[0].cities[2] = {
-        name: "Красноярск",
-        population: 1007000
-    }
-
-    countries[1] = {
-        name: "Англия",
-        cities: []
-    }
-
-    countries[1].cities[0] = {
-        name: "Ливерпуль",
-        population: 496784
-    }
-
-    countries[1].cities[1] = {
-        name: "Манчестер",
-        population: 553230
-    }
-
-    countries[1].cities[2] = {
-        name: "Лондон",
-        population: 8982000
-    }
-
-    countries[1].cities[3] = {
-        name: "Бристоль",
-        population: 428234
-    }
-
-    countries[2] = {
-        name: "Италия",
-        cities: []
-    }
-
-    countries[2].cities[0] = {
-        name: "Палермо",
-        population: 657960
-    }
-
-    countries[2].cities[1] = {
-        name: "Милан",
-        population: 1396059
-    }
-
-    var maxCitiesCount = 0;
-
-    countries.forEach(country => {
-        if (country.cities.length >= maxCitiesCount) {
-            maxCitiesCount = country.cities.length;
+            return countries
+                .filter(function (country) {
+                    return country.cities.length === maxCitiesCount;
+                })
+                .map(function (country) {
+                    return country.name;
+                });
         }
-    })
 
-    console.log(maxCitiesCount);
+        console.log("Страны с наибольшим количеством городов: [" + getMaxCitiesCountCountries() + "]");
 
-    var maxCitiesCountCountries = countries
-        .filter(function (country) {
-            return country.cities.length === maxCitiesCount;
-        })
-        .map(function (country) {
-            return country.name;
-        });
+        function getTotalCountriesPopulation() {
+            var totalCountriesPopulation = {};
 
-    console.log("Страны с наибольшим количеством городов: " + maxCitiesCountCountries);
+            countries.forEach((country) => {
+                var countryName = country.name;
 
-    var totalCountriesPopulation = countries
-        .map(function (country) {
-            return country = {
-                [country.name]: country.cities.reduce(function (sum, city) {
+                totalCountriesPopulation[countryName] = country.cities.reduce(function (sum, city) {
                     return sum + city.population;
                 }, 0)
-            }
-        });
+            })
 
-    console.log("Суммарная численность населения по странам: ", totalCountriesPopulation);
-})();
+            return totalCountriesPopulation;
+        }
+
+        console.log("Суммарная численность населения по странам: ", getTotalCountriesPopulation());
+    }
+)
+();
