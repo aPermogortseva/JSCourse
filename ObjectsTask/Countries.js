605c7c83f3f7166ad50357af45827295d455e85d
aPermogortseva <dm3gGjPZQ67Tmke>
2022-02-17 20:23:32 +0700

    Внесены исправления в задачу Работа с объектами

--- ObjectsTask/Countries.js
+++ ObjectsTask/Countries.js
@@ -1,101 +1,98 @@
 (function () {
-        var countries = [
-            {
-                name: "Россия",
-                cities: [
-                    {
-                        name: "Новосибирск",
-                        population: 1511000
-                    },
-                    {
-                        name: "Омск",
-                        population: 1159000
-                    },
-                    {
-                        name: "Красноярск",
-                        population: 1007000
-                    }
-                ]
-            },
-            {
-                name: "Англия",
-                cities: [
-                    {
-                        name: "Ливерпуль",
-                        population: 496784
-                    },
-                    {
-                        name: "Манчестер",
-                        population: 553230
-                    },
-                    {
-                        name: "Лондон",
-                        population: 8982000
-                    },
-                    {
-                        name: "Бристоль",
-                        population: 428234
-                    }
-                ]
-            },
-            {
-                name: "Италия",
-                cities: [
-                    {
-                        name: "Палермо",
-                        population: 657960
-                    },
-                    {
-                        name: "Милан",
-                        population: 1396059
-                    },
-                    {
-                        name: "Милан",
-                        population: 1396059
-                    },
-                    {
-                        name: "Милан",
-                        population: 1396059
-                    }
-                ]
-            }
-        ];
-
-        function getMaxCitiesCountCountries() {
-            var maxCitiesCount = countries.reduce(function (max, current) {
-                if (current.cities.length > max) {
-                    max = current.cities.length;
+    var countries = [
+        {
+            name: "Россия",
+            cities: [
+                {
+                    name: "Новосибирск",
+                    population: 1511000
+                },
+                {
+                    name: "Омск",
+                    population: 1159000
+                },
+                {
+                    name: "Красноярск",
+                    population: 1007000
                 }
-
-                return max;
-            }, 0);
-
-            return countries
-                .filter(function (country) {
-                    return country.cities.length === maxCitiesCount;
-                })
-                .map(function (country) {
-                    return country.name;
-                });
+            ]
+        },
+        {
+            name: "Англия",
+            cities: [
+                {
+                    name: "Ливерпуль",
+                    population: 496784
+                },
+                {
+                    name: "Манчестер",
+                    population: 553230
+                },
+                {
+                    name: "Лондон",
+                    population: 8982000
+                },
+                {
+                    name: "Бристоль",
+                    population: 428234
+                }
+            ]
+        },
+        {
+            name: "Италия",
+            cities: [
+                {
+                    name: "Палермо",
+                    population: 657960
+                },
+                {
+                    name: "Милан",
+                    population: 1396059
+                },
+                {
+                    name: "Милан",
+                    population: 1396059
+                },
+                {
+                    name: "Милан",
+                    population: 1396059
+                }
+            ]
         }
+    ];
 
-        console.log("Страны с наибольшим количеством городов: [" + getMaxCitiesCountCountries() + "]");
+    function getMaxCitiesCountCountries(countries) {
+        var maxCitiesCuntInCurrentCountry = 0;
 
-        function getTotalCountriesPopulation() {
-            var totalCountriesPopulation = {};
+        var maxCitiesCount = countries.reduce(function (max, currentCountry) {
+            if (currentCountry.cities.length > maxCitiesCuntInCurrentCountry) {
+                maxCitiesCuntInCurrentCountry = currentCountry.cities.length;
+            }
 
-            countries.forEach((country) => {
-                var countryName = country.name;
+            return maxCitiesCuntInCurrentCountry;
+        }, 0);
 
-                totalCountriesPopulation[countryName] = country.cities.reduce(function (sum, city) {
-                    return sum + city.population;
-                }, 0)
-            })
-
-            return totalCountriesPopulation;
-        }
-
-        console.log("Суммарная численность населения по странам: ", getTotalCountriesPopulation());
+        return countries
+            .filter(function (country) {
+                return country.cities.length === maxCitiesCount;
+            });
     }
-)
-();
+
+    console.log("Страны с наибольшим количеством городов: ", getMaxCitiesCountCountries(countries));
+
+    function getTotalCountriesPopulation(countries) {
+        var totalCountriesPopulation = {};
+
+        countries.forEach(function (country) {
+            var countryName = country.name;
+
+            totalCountriesPopulation[countryName] = country.cities.reduce(function (sum, city) {
+                return sum + city.population;
+            }, 0);
+        })
+
+        return totalCountriesPopulation;
+    }
+
+    console.log("Суммарная численность населения по странам: ", getTotalCountriesPopulation(countries));
+})();
