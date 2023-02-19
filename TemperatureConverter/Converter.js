ed86a4086dd761056c57d00761e74c57840dc2c0
aPermogortseva <dm3gGjPZQ67Tmke>
2023-02-20 02:18:13 +0700

    внесены исправления в задачу Перевод Температур

--- TemperatureConverter/Converter.js
+++ TemperatureConverter/Converter.js
@@ -1,109 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var inputValue = document.getElementById("celsius-temperature-input");
-    var dotCount = 0;
+    var input = document.getElementById("celsius-temperature-input");
 
-    inputValue.addEventListener("keydown", function (e) {
-        checkInput(e);
-
-        if (inputValue.value.length === 1) {
+    input.addEventListener("keydown", function (e) {
+        if (input.value.length === 1) {
             checkFirstSymbolForDot();
         }
-
-        checkExtraHyphens(e);
-
-        checkExtraDots(e);
-
-        updateDotCount(e);
     });
 
-    function updateDotCount(e) {
-        var lastSymbol = inputValue.value.charAt(inputValue.value.length - 1);
-
-        if (e.key === 'Backspace' && lastSymbol === '.') {
-            dotCount--;
-        }
-    }
-
-    function checkInput(e) {
-        if ((e.key < '0' || e.key > '9') && e.key !== '-' && e.key !== 'ArrowLeft' && e.key !== 'ArrowRight' &&
-            e.key !== 'Delete' && e.key !== 'Backspace' && e.key !== '.' && e.key &&
-            !(e.ctrlKey && e.code === 'KeyV') && !(e.ctrlKey && e.code === 'KeyX') &&
-            !(e.ctrlKey && e.code === 'KeyC')) {
-            e.preventDefault();
-        }
-    }
-
-    function checkExtraHyphens(e) {
-        if (e.key === '-' && inputValue.value.length >= 1) {
-            e.preventDefault();
-        }
-    }
-
-    function checkExtraDots(e) {
-        if (e.key === '.') {
-            if (dotCount > 0) {
-                e.preventDefault();
-            } else {
-                dotCount++;
-            }
-        }
-    }
-
     function checkFirstSymbolForDot() {
-        if (inputValue.value.charAt(0) === '.') {
-            dotCount++;
-            return inputValue.value = '0' + inputValue.value;
+        if (input.value.charAt(0) === ".") {
+            return input.value = "0" + input.value;
         }
 
-        return inputValue.value;
+        return input.value;
     }
 
-    function checkIsEmpty(value) {
-        return value.length === 0;
-    }
-
-    function trimDotAndHyphen(value) {
-        if (checkIsEmpty(value) === false) {
-            var lastSymbol = value.charAt(value.length - 1);
-
-            if (lastSymbol === '-') {
-                return value.slice(0, -1);
-            }
-
-            if (lastSymbol === '.') {
-                dotCount--;
-
-                return value.slice(0, -1);
-            }
-        }
-
-        return value;
+    function checkIsEmpty(string) {
+        return string.length === 0;
     }
 
     function convert() {
         var convertButton = document.getElementById("convert-button");
 
         convertButton.addEventListener("click", function () {
-            inputValue.value = trimDotAndHyphen(inputValue.value);
+            input.value = input.value.replace("е", "e");
+            input.value = input.value.replace(",", ".");
 
-            inputValue.parentElement.classList.remove("invalid");
+            input.parentElement.classList.remove("invalid");
 
-            if (checkIsEmpty(inputValue.value)) {
-                inputValue.parentElement.classList.add("invalid");
+            if (checkIsEmpty(input.value)) {
+                input.parentElement.classList.add("invalid");
                 return;
             }
 
-            if (isNaN(inputValue.value)) {
-                inputValue.parentElement.classList.add("invalid");
+            if (isNaN(input.value)) {
+                input.parentElement.classList.add("invalid");
                 return;
             }
 
-            var celsiusValue = inputValue.value;
+            var celsiusTemperature = parseFloat(input.value);
 
-            document.getElementById("kelvin-temperature").value = (parseInt(celsiusValue, 10) + 273.15).toFixed(2);
+            document.getElementById("kelvin-temperature").value = (celsiusTemperature + 273.15).toFixed(2);
 
-            document.getElementById("fahrenheit-temperature").value = ((parseInt(celsiusValue, 10) * 1.8) + 32).toFixed(2);
-        })
+            document.getElementById("fahrenheit-temperature").value = ((celsiusTemperature * 1.8) + 32).toFixed(2);
+        });
     }
 
     convert();
